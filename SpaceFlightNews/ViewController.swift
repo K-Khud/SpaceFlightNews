2ade0c4a10e36cf20a542d6997726e965f5ba30a
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2021-03-13 12:59:31 +0200

    Initial Commit

---
+++ SpaceFlightNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceFlightNews
+//
+//  Created by Ekaterina Khudzhamkulova on 13.3.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
